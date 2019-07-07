f1ce3cc257f4d313519640b4c7b043b787df1d05
Minsoo Kim <leios76@gmail.com>
2019-07-07 17:56:56 +0900

    layer 배포 방식 적용

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ rm -f ${NAME}.zip
 case ${REVISION} in
     *develop*)
         cd src && \
-            zip -r ../${NAME}.zip . && \
+            zip -r ../${NAME}.zip . -x node_modules\* && \
             cd .. && \
             aws lambda update-function-code --function-name ${NAME} --zip-file fileb://${NAME}.zip
         ;;
     *)
         cd src && \
-            zip -r ../${NAME}.zip . && \
+            zip -r ../${NAME}.zip . -x node_modules\* && \
             cd .. && \
             aws lambda update-function-code --function-name ${NAME} --zip-file fileb://${NAME}.zip && \
             VERSION=$(aws lambda publish-version --function-name ${NAME} | jq -r .Version) && \
